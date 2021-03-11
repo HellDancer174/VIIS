9cbc9ebb12aaa8a2c5aa4cdba03a01028b07c3d6
HellDancer174 <viktorignatev873@gmail.com>
2021-03-11 13:09:30 +0500

    Переименовал проект VIIS в VIIS.App

--- VIIS/MainWindow.xaml.cs
+++ VIIS/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace VIIS
+namespace VIIS.App
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
